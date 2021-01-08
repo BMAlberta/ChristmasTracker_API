53bdc26e83817b9a9523fc96449e17c36c24b04c
Brian <brian.m.alberta@gmail.com>
2021-01-08 23:15:44 +0000

    Adding ignore file and removing logs

---
+++ models/item.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: false,
+        default: 3
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: String,
+        required: true,
+        default: "No ID"
+    },
+    purchased: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    purchasedBy: {
+        type: String,
+        required: false
+    },
+    creationDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    lastEditDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Item', itemSchema)
