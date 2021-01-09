4f80f12a6bcb3d38c3c369ed1f3f1c980cc37624
Brian <brian.m.alberta@gmail.com>
2021-01-09 20:03:10 +0000

    Add payload model

---
+++ models/payload.js
@@ -0,0 +1,26 @@
+function Response(error = null, payload) {
+    if (error != null) {
+        this.error = { message: error }
+    } else {
+      this.error = {}
+    }
+
+    if (payload != null) {
+        this.payload = payload
+      } else {
+        this.payload = {}
+      }
+}
+
+
+function DataResponse(payload) {
+    return new Response(null, payload)
+
+}
+
+
+function ErrorResponse(message) {
+    return new Response(message, null)
+}
+
+module.exports = { Response, DataResponse, ErrorResponse }
