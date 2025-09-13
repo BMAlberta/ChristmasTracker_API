026e4a23535278e4f51e91c2615b3dd8fc50b20c
Brian Alberta <brian.m.alberta@gmail.com>
2025-09-12 21:36:04 -0400

    BMCT-26: Create dataRequest manager.

---
+++ src/util/dataRequest.mjs
@@ -0,0 +1,89 @@
+import { getDbConnection } from "../config/db.mjs";
+
+export const ProcedureType = Object.freeze({
+    ACTIVE_MEMBER_LISTS: "getActiveMemberLists",
+    JOINED_LISTS: "getJoinedLists",
+    LOGIN_INFO: "CALL tracker.getLoginInfo(?)",
+    MEMBER_LIST_OVERVIEWS: "getMemberListsOverviews",
+    PURCHASE_SUMMARY_BY_LIST: "getPurchaseSummaryByListForUser",
+    USER_DETAILS: "CALL tracker.getUserDetails(?)",
+    USER_ID: "getUserId",
+    UPDATE_USER_RETURN_INFO: "updateAndReturnUserInfo",
+    UPDATE_LOGIN_INFO: "CALL tracker.updateLastLoginDetails(?,?,?)",
+    UPDATE_USER_INFO: "updateUserInfo",
+    OWNED_LISTS: "getOwnedLists",
+    UPDATE_PASSWORD: "CALL tracker.updatePassword(?,?)",
+    PASSWORD_INFO: "CALL tracker.getPasswordInfo(?)",
+    CREATE_USER: "CALL tracker.createUser(?,?,?,?)",
+    CHECK_USER: "CALL tracker.checkForUserByEmail(?)",
+    DELETE_USER: "CALL tracker.deleteUserById(?)"
+});
+
+// function formatDataRequest(procedureName) {
+//     return `CALL tracker.${procedureName}(?)`
+// }
+
+
+export async function makeFormattedDataRequest(procedureName, data) {
+    try {
+        let connection = await getDbConnection();
+        let result = await connection.query(procedureName, data);
+        return result[0];
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function findOne(procedureName, data) {
+    try {
+        let result = await makeFormattedDataRequest(procedureName, data);
+        if (result.length == 0) {
+            throw Error('No result found.');
+        }
+        return result[0];
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function findMany(procedureName, data) {
+    try {
+        let result = await makeFormattedDataRequest(procedureName, data);
+        if (result.length == 0) {
+            throw Error('No result found.');
+        }
+        return result;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function updateOne(procedureName, data) {
+    try {
+        let result = await makeFormattedDataRequest(procedureName, data);
+        return
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function createOne(procedureName, data) {
+    try {
+        let result = await makeFormattedDataRequest(procedureName, data);
+        if (result.length == 0) {
+            throw Error('No result found.');
+        }
+        return result[0];
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export async function deleteOne(procedureName, data) {
+    try {
+        let result = await makeFormattedDataRequest(procedureName, data);
+        return
+    } catch (err) {
+        console.log(err)
+    }
+}
